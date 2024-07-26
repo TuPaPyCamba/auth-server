b91ef70d43ac851d9bedefb9403e963b056e9767
Camba <fercambafcm@gmail.com>
2024-07-26 16:16:31 -0600

    update

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-// models/User.js
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -17,13 +17,13 @@ const userSchema = new mongoose.Schema({
 
 // Método para cifrar la contraseña antes de guardar
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+    if (!this.isModified('password')) return next()
 
     const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
 
 export default User;
--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import session from 'express-session'
-import mongoose from 'mongoose'
+import express from 'express';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import 'colors';
+import { PORT, MONGODB_URI, SECRET } from './config.js';
+import User from './models/user.js'; 
 
-import 'colors'
-import { PORT, MONGODB_URI, SECRET } from './config.js'
-import User from './models/user.js'
+const server = express();
 
-const server = express()
-
-
-// Conexion a la BD MongoDB Atlas
+// Conexión a la BD MongoDB Atlas
 mongoose.connect(MONGODB_URI)
-    .then(() => console.log('SERVER:'.green + `Successful connection to `+`MONGOOSE`.yellow +` database`))
+    .then(() => console.log('SERVER:'.green + ` Successful connection to ` + `MONGOOSE`.yellow + ` database \n`))
     .catch(err => console.error('Error de conexión:', err));
 
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 
 // Configuración de sesión
 server.use(session({
@@ -30,63 +29,72 @@ server.post('/register', async (req, res) => {
     const { username, password } = req.body;
 
     try {
-        Validation.username(username)
-        Validation.password(password)
+        Validation.username(username);
+        Validation.password(password);
 
         const userExistence = await User.findOne({ username });
-        if (userExistence) return res.status(400).send({ error: 'username already exists' })
+        if (userExistence) return res.status(400).send({ error: 'username already exists' });
 
-        const user = new User({ username, password })
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({ username, password: hashedPassword });
         await user.save();
-        res.status(201).send({ message: 'Usuario creado', userId: user._id })
-        console.log(`SERVER:`.green + ` New user created \n ID: ${id} \n Username: ${username} \n Password: ${hasedPassword} \n`)
+        res.status(201).send({ message: 'Usuario creado', userId: user._id });
+        console.log(`SERVER:`.green + ` New user created \n ID: ${user._id} \n Username: ${username} \n Password: ***** \n`);
     } catch (error) {
         res.status(400).send({ error: 'Error al crear el usuario', details: error.message });
         console.log(
             `SERVER:`.green + ` Error when trying to create a new User, with the following data \n Username: ${username} \n Password: ***** \n ` + `ESTATUS: (400) `.red + `${error.message} \n`
-        )
-    }
-});
-
-server.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-
-        Validation.username(username)
-        Validation.password(password)
-
-        const user = await User.findOne({ username });
-        if (!user) return res.status(400).send({ error: 'username does not exist' })
-
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(400).send({ error: 'password is invalid' })
-
-        // Aquí puedes manejar la creación de una sesión si es necesario
-        res.send({ message: 'Inicio de sesión exitoso', userId: user._id });
-    } catch (error) {
-        res.status(500).send({ error: 'Error en el servidor', details: error.message })
-        console.log(
-            `SERVER:`.green + ` Error when trying to login into an acount, with the following data \n Username: ${username} \n Password: ***** \n ` + `ESTATUS: (401)`.red + ` ${error.message} \n`
         );
     }
 });
 
-// Validacion de campos
+// Ruta para iniciar sesión
+server.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        Validation.username(username);
+        Validation.password(password);
+
+        const user = await User.findOne({ username });
+        if (!user) {
+            const errorMessage = 'username does not exist';
+            console.log(`SERVER:`.green + ` Error when trying to login into an acount, with the following data \n Username: ${username} \n Password: ***** \n ` + `ESTATUS: (401)`.red + ` ${error.message} \n`);
+            return res.status(400).send({ error: errorMessage });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            const errorMessage = 'password is invalid';
+            console.log(`SERVER:`.green + ` Error when trying to login into an acount, with the following data \n Username: ${username} \n Password: ***** \n ` + `ESTATUS: (401)`.red + ` ${error.message} \n`);
+            return res.status(400).send({ error: errorMessage });
+        } 
+
+        // Aquí puedes crear la sesión si es necesario
+        res.send({ message: 'Inicio de sesión exitoso', userId: user._id });
+    } catch (error) {
+        res.status(500).send({ error: 'Error en el servidor', details: error.message });
+        console.log(
+            `SERVER:`.green + ` Error when trying to login into an account, with the following data \n Username: ${username} \n Password: ***** \n ` + `ESTATUS: (401)`.red + ` ${error.message} \n`
+        );
+    }
+});
+
+// Validación de campos
 class Validation {
     static username(username) {
-        if (typeof username !== 'string') throw new Error('username must be a string')
-        if (username.length < 5) throw new Error('username must be at least 5 characters long')
+        if (typeof username !== 'string') throw new Error('username must be a string');
+        if (username.length < 5) throw new Error('username must be at least 5 characters long');
     }
 
     static password(password) {
-        if (typeof password !== 'string') throw new Error('password must be a string')
-        if (password.length < 8) throw new Error('password must be at least 8 characters long')
+        if (typeof password !== 'string') throw new Error('password must be a string');
+        if (password.length < 8) throw new Error('password must be at least 8 characters long');
     }
 }
 
-// Inicializacion del Servidor 
+// Inicialización del Servidor 
 server.listen(PORT, () => {
-
-    console.log(`SERVER: `.green + `Authentication services running correctly on the following port \n \n service rute:  ` + `http://localhost:${PORT} \n`.blue)
-})
+    console.log(`SERVER: `.green + `Authentication services running correctly on the following port \n \n service route:  ` + `http://localhost:${PORT} \n`.blue);
+});
