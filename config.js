f13787fa1802e0f6d495e59e7cad256f0b91f94d
Camba <fercambafcm@gmail.com>
2024-07-29 17:40:47 -0600

    update

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ export const {
     PORT = 3000,
     MONGODB_URI = 'mongodb+srv://fercambafcm:SYgO2AnL8zV7Xdhq@cluster0.wnwgkr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     SECRET = 'tu_secreto_aqui',
-    GMAIL = ''
+    GMAIL = 'anonimo2024cod117@gmail.com'
 } = process.env
